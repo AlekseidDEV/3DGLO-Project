284e379f55dbc80a1a5e150c723485ff5438832b
AleksDev <lesha.rudiko@gmail.com>
2023-08-26 13:18:00 +0300

    обновил таймер, добавил подписи ко времени, и функцию склонения таймера

--- src/index.js
+++ src/index.js
@@ -1,3 +1,3 @@
 import timer from "./modules/timer"
 
-timer("23 August 2023 21:55:00")
+timer("28 August 2023 21:55:00")
--- src/modules/timer.js
+++ src/modules/timer.js
@@ -6,6 +6,16 @@ const timer = (dedlain) => {
 
   let idInterval;
 
+  const numWords = (value, words) => {
+    value = Math.abs(value) % 100
+    const lastValeu = value % 10
+
+    if((value > 10) && (value < 20)) return words[2]
+    if((lastValeu > 1) && (lastValeu < 5)) return words[1]
+    if(lastValeu === 1) return words[0]
+    return words[2]
+  }
+
   const getTimeRemain = () => {
     let dateNow = new Date().getTime();
     let dateStop = new Date(dedlain).getTime();
@@ -22,10 +32,10 @@ const timer = (dedlain) => {
   const updateClock = () => {
     const { second, minute, hourse, day, timeRemain } = getTimeRemain();
 
-    spanSecond.textContent = String(second).padStart(2, "0");
-    spanMinute.textContent = String(minute).padStart(2, "0");
-    spanHourse.textContent = String(hourse).padStart(2, "0");
-    spanDay.textContent = String(day).padStart(2, "0");
+    spanSecond.textContent = String(second).padStart(2, "0")
+    spanMinute.textContent = String(minute).padStart(2, "0")
+    spanHourse.textContent = String(hourse).padStart(2, "0")
+    spanDay.textContent = String(day).padStart(2, "0")
 
     if (timeRemain <= 0) {
       spanDay.textContent = "00";
@@ -34,9 +44,34 @@ const timer = (dedlain) => {
       spanSecond.textContent = "00";
       clearInterval(idInterval);
     }
+
+    spanSecond.nextElementSibling.textContent = numWords(second, [
+      'секунда',
+      'секунды',
+      'секунд'
+    ])
+
+    spanMinute.nextElementSibling.textContent = numWords(minute, [
+      'минута',
+      'минуты',
+      'минут'
+    ])
+
+    spanHourse.nextElementSibling.textContent = numWords(hourse, [
+      'час',
+      'часа',
+      'часов'
+    ])
+
+    spanDay.nextElementSibling.textContent = numWords(day, [
+      'день',
+      'дня',
+      'дней'
+    ])
   };
 
   idInterval = setInterval(updateClock, 1000);
 };
+
 
 export default timer;
