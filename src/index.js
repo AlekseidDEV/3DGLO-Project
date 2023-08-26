4f939a890893a058e775d25a43dbc43f63a9171d
AleksDev <lesha.rudiko@gmail.com>
2023-08-26 19:34:22 +0300

    новая ветка, здесь будет модальное окно и меню

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+import menu from "./modules/menu"
 import timer from "./modules/timer"
 
 timer("28 August 2023 21:55:00")
+menu()
---
+++ src/modules/menu.js
@@ -0,0 +1,5 @@
+const menu = () => {
+    console.log('hello world');
+}
+
+export default menu
