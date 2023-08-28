560bc07c3b22f4288e0297fd111cf42c1569582f
AleksDev <lesha.rudiko@gmail.com>
2023-08-28 06:58:52 +0300

    небольшие правки

--- src/modules/smoothScroll.js
+++ src/modules/smoothScroll.js
@@ -1,30 +1,32 @@
 const smoothScroll = () => {
-    const btnScroll = document.querySelector('a[href = "#service-block"]')
-    const menu = document.querySelector('menu')
-    const itemsMenu = menu.querySelectorAll('a')
+  const btnScroll = document.querySelector('a[href = "#service-block"]');
+  const menu = document.querySelector("menu");
+  const itemsMenu = menu.querySelectorAll("a");
 
-    const hendlerScroll = (e) => {
-        e.preventDefault()
-       document.getElementById('service-block').scrollIntoView({
-        behavior : 'smooth', 
-        block : 'start'
-       })
-    }
+  const hendlerScroll = (e) => {
+    e.preventDefault();
+    document.getElementById("service-block").scrollIntoView({
+      behavior: "smooth",
+      block: "start",
+    });
+  };
 
-    const smoothLink = (item) => {
-        const blockId = item.getAttribute('href').slice(1)
-        
-        document.getElementById(blockId).scrollIntoView({
-            behavior : 'smooth',
-            block : 'start'
-        })
-    }
-    
-    itemsMenu.forEach((item) => item.addEventListener('click', (e) => {
-        e.preventDefault()
-        smoothLink(item)
-    }))
-    btnScroll.addEventListener('click', hendlerScroll)
-}
+  const smoothLink = (item) => {
+    const blockId = item.getAttribute("href").slice(1);
 
-export default smoothScroll
+    document.getElementById(blockId).scrollIntoView({
+      behavior: "smooth",
+      block: "start",
+    });
+  };
+
+  itemsMenu.forEach((item) =>
+    item.addEventListener("click", (e) => {
+      e.preventDefault();
+      smoothLink(item);
+    })
+  );
+  btnScroll.addEventListener("click", hendlerScroll);
+};
+
+export default smoothScroll;
