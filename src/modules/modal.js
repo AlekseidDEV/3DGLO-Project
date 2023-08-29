7af6b87f08f2749de1167b0eccdf24aafe5eeb8e
AleksDev <lesha.rudiko@gmail.com>
2023-08-29 09:55:22 +0300

    update

--- src/modules/modal.js
+++ src/modules/modal.js
@@ -1,38 +1,38 @@
 const modal = () => {
-  const popupBtn = document.querySelectorAll(".popup-btn");
-  const modal = document.querySelector(".popup");
-  const btnPopupClose = modal.querySelector(".popup-close");
+    const popupBtn = document.querySelectorAll(".popup-btn");
+    const modal = document.querySelector(".popup");
+    const btnPopupClose = modal.querySelector(".popup-close");
 
-  let animateModal;
-  let count = -100;
+    let animateModal;
+    let count = -100;
 
-  const openModalWindow = () => {
-    count++;
-    animateModal = requestAnimationFrame(openModalWindow);
+    const openModalWindow = () => {
+        count++;
+        animateModal = requestAnimationFrame(openModalWindow);
 
-    if (count <= 0 && innerWidth >= 768) {
-      modal.style.transform = `translateX(${count * 15}px)`;
-    } else {
-      cancelAnimationFrame(animateModal);
-      count = -100;
-    }
-  };
+        if (count <= 0 && innerWidth >= 768) {
+            modal.style.transform = `translateX(${count * 15}px)`;
+        } else {
+            cancelAnimationFrame(animateModal);
+            count = -100;
+        }
+    };
 
-  const closeModal = (e) => {
-    if (e.target.className === "popup-close") {
-      modal.style.transform = "translateX(-100%)";
-    } else if (e.target.className === "popup") {
-      modal.style.transform = "translateX(-100%)";
-    }
-  };
+    const closeModal = e => {
+        if (e.target.className === "popup-close") {
+            modal.style.transform = "translateX(-100%)";
+        } else if (e.target.className === "popup") {
+            modal.style.transform = "translateX(-100%)";
+        }
+    };
 
-  popupBtn.forEach((button) => {
-    button.addEventListener("click", openModalWindow);
-  });
+    popupBtn.forEach(button => {
+        button.addEventListener("click", openModalWindow);
+    });
 
-  btnPopupClose.addEventListener("click", closeModal);
+    btnPopupClose.addEventListener("click", closeModal);
 
-  modal.addEventListener("click", closeModal);
+    modal.addEventListener("click", closeModal);
 };
 
 export default modal;
