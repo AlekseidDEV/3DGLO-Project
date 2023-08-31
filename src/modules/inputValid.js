e8dc353bdb5881e37c1cbc178ea15afed60383eb
AleksDev <lesha.rudiko@gmail.com>
2023-08-31 10:58:27 +0300

    валидация инпутов v.2

--- src/modules/inputValid.js
+++ src/modules/inputValid.js
@@ -1,28 +1,23 @@
 const inputValid = () => {
     const selectForm = document.querySelector('.calc-item.calc-type')
+    const inputsValid = document.querySelectorAll('input[id]')
     const inputSquare = document.querySelector('.calc-item.calc-square')
     const inputCalc = document.querySelector('.calc-item.calc-count')
     const inputCalcDay = document.querySelector('.calc-item.calc-day')
-    const nameInputHeader = document.getElementById('form1-name')
-    const nameInputFooter = document.getElementById('form2-name')
-    const userInputMessege = document.getElementById('form2-message')
-    const inputEmailHeader = document.getElementById('form1-email')
-    const inputEmailFooter = document.getElementById('form2-email')
-    const inputPhoneHeader = document.getElementById('form1-phone')
-    const inputPhoneFooter = document.getElementById('form2-phone')
 
+    inputsValid.forEach((input) => {
+        const validInput = (e) => {
+            if(e.target.name === 'user_name' || e.target.name === 'user_message'){
+                e.target.value = e.target.value.replace(/[^а-яА-Я\s-]+/g, '')
+            }  else if (e.target.name === 'user_email'){
+                e.target.value = e.target.value.replace(/[^-a-zA-Z@_.!~*']/g, '')
+            }  else if (e.target.name === 'user_phone'){
+                e.target.value = e.target.value.replace(/[^-0-9()]/g, '')
+            }
+        }
 
-    const textValid = (e) => {
-        e.target.value = e.target.value.replace(/[^а-яА-Я\s-]+/g, '');
-    }
-
-    const emailValid = (e) => {
-        e.target.value = e.target.value.replace(/[^-a-zA-Z@_.!~*']/g, '')
-    }
-
-    const phoneNumberValid = (e) => {
-        e.target.value = e.target.value.replace(/[^-0-9()]/g, '')
-    }
+        input.addEventListener('input' , validInput)
+    })
 
     const cleanInput = (value) => {
         if(value === '1' || value === '2' || value === '3'){
@@ -31,10 +26,6 @@ const inputValid = () => {
             inputCalcDay.value = ''
         }
     }
-    
-    selectForm.addEventListener('change', (e) => {
-        cleanInput(e.target.value)
-    })
 
     inputSquare.addEventListener('input', (e) => {
         e.target.value = e.target.value.replace(/^\D/g , '')
@@ -46,13 +37,9 @@ const inputValid = () => {
         e.target.value = e.target.value.replace(/^\D/g , '')
     })
 
-    nameInputHeader.addEventListener('input' , textValid)
-    nameInputFooter.addEventListener('input', textValid)
-    userInputMessege.addEventListener('input', textValid)
-    inputEmailHeader.addEventListener('input', emailValid)
-    inputEmailFooter.addEventListener('input', emailValid)
-    inputPhoneHeader.addEventListener('input', phoneNumberValid)
-    inputPhoneFooter.addEventListener('input', phoneNumberValid)
+    selectForm.addEventListener('change', (e) => {
+        cleanInput(e.target.value)
+    })
 }
 
 export default inputValid
