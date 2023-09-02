f5e2a96e0b62fd4160a102a556f449d380f61787
AleksDev <lesha.rudiko@gmail.com>
2023-09-02 09:46:14 +0300

    валидация форм v.3

--- src/modules/inputValid.js
+++ src/modules/inputValid.js
@@ -1,23 +1,22 @@
 const inputValid = () => {
-  const selectForm = document.querySelector(".calc-item.calc-type");
-  const inputsValid = document.querySelectorAll("input[id]");
+  const select = document.querySelector(".calc-item.calc-type");
   const inputSquare = document.querySelector(".calc-item.calc-square");
   const inputCalc = document.querySelector(".calc-item.calc-count");
   const inputCalcDay = document.querySelector(".calc-item.calc-day");
 
-  inputsValid.forEach((input) => {
-    const validInput = (e) => {
-      if (e.target.name === "user_name" || e.target.name === "user_message") {
-        e.target.value = e.target.value.replace(/[^а-яА-Я\s-]+/g, "");
-      } else if (e.target.name === "user_email") {
-        e.target.value = e.target.value.replace(/[^-a-zA-Z@_.!~*']/g, "");
-      } else if (e.target.name === "user_phone") {
-        e.target.value = e.target.value.replace(/[^-0-9()]/g, "");
-      }
-    };
-
-    input.addEventListener("input", validInput);
-  });
+  const validInput = (e) => {
+    const calcInput = e.target.classList[0];
+    const userInfoInput = e.target.name;
+    if (userInfoInput === "user_name" || userInfoInput === "user_message") {
+      e.target.value = e.target.value.replace(/[^а-яА-Я\s-]+/g, "");
+    } else if (userInfoInput === "user_email") {
+      e.target.value = e.target.value.replace(/[^-a-zA-Z@_.!~*']/g, "");
+    } else if (userInfoInput === "user_phone") {
+      e.target.value = e.target.value.replace(/[^-0-9()]/g, "");
+    } else if (calcInput === "calc-item") {
+      e.target.value = e.target.value.replace(/^\D/g, "");
+    }
+  };
 
   const cleanInput = (value) => {
     if (value === "1" || value === "2" || value === "3") {
@@ -27,18 +26,11 @@ const inputValid = () => {
     }
   };
 
-  inputSquare.addEventListener("input", (e) => {
-    e.target.value = e.target.value.replace(/^\D/g, "");
-  });
-  inputCalc.addEventListener("input", (e) => {
-    e.target.value = e.target.value.replace(/^\D/g, "");
-  });
-  inputCalcDay.addEventListener("input", (e) => {
-    e.target.value = e.target.value.replace(/^\D/g, "");
-  });
-
-  selectForm.addEventListener("change", (e) => {
-    cleanInput(e.target.value);
+  document.addEventListener("input", validInput);
+  select.addEventListener("change", (e) => {
+    if (e.target.nodeName === "SELECT") {
+      cleanInput(e.target.value);
+    }
   });
 };
 
