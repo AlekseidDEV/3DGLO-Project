88f053e1cabd2ce15c6f3d040c9346ed59a1328f
AleksDev <lesha.rudiko@gmail.com>
2023-09-19 11:18:47 +0300

    update

--- src/modules/calc.js
+++ src/modules/calc.js
@@ -1,4 +1,5 @@
 import { animate } from "./helper";
+
 const calc = (price = 100) => {
   const calcBlock = document.querySelector(".calc-block");
   const calcType = document.querySelector(".calc-type");
